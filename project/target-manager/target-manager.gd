27a0a6f89c901a26de0ae3a6744fdd7865000dcc
tanguy <tanguy.escaron@hotmail.fr>
2022-02-24 20:12:45 +0100

    wip targets far

--- project/target-manager/target-manager.gd
+++ project/target-manager/target-manager.gd
@@ -2,15 +2,38 @@ extends Spatial
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var max_x: int = 50
+var max_y: int = 50
 export (PackedScene) var target_scene: PackedScene
 
+var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(10):
+	#for i in range(1000):
+	#	var a = target_scene.instance()
+	#	a.pos_bound = 20
+	#	a.pos_min = 0
+	#a.rot_bound = 0.5
+	#	a.speed_bound = 0.1
+	#	a.transform.origin.x = rng.randf_range(-max_x, max_x)
+	#	a.transform.origin.y = rng.randf_range(-max_y, max_y)
+	#	add_child(a)
+	for i in range(100):
 		var a = target_scene.instance()
+		a.pos_bound = 20
+		a.pos_min = 100
+		a.rot_bound = 0.2
+		a.speed_bound = 0.04
+		a.global_scale(Vector3(5, 5, 5))
 		add_child(a)
+	#for i in range(10):
+#		var a = target_scene.instance()
+#		a.pos_bound = 20
+#		a.pos_min = 3000
+#		a.rot_bound = 0.5
+#		a.speed_bound = 0.05
+#		a.global_scale(Vector3(50, 50, 50))
+#		add_child(a)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- project/target/target.gd
+++ project/target/target.gd
@@ -5,9 +5,19 @@ var rng = RandomNumberGenerator.new()
 # Declare member variables here. Examples:
 
 onready var object : Spatial = $Race
+
+export (float) var speed_bound: float
+export (float) var pos_min: float
+export (float) var pos_bound: float
+export (float) var rot_bound: float
+
 var speed_x
 var speed_y
 var speed_z
+
+var pos_x
+var pos_y
+var pos_z
 
 var rot_x
 var rot_y
@@ -16,19 +26,25 @@ var rot_z
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
-	var my_random_number = rng.randf_range(-10.0, 10.0)
+	pos_x = pos_min + pos_bound * rng.randfn()
+	pos_y = pos_min + pos_bound * rng.randfn()
+	pos_z = pos_min + pos_bound * rng.randfn()
 	
-	object.transform.origin.x = rng.randf_range(-20.0, 20.0)
-	object.transform.origin.y = rng.randf_range(-20.0, 20.0)
-	object.transform.origin.z = rng.randf_range(-20.0, 20.0)
+	object.transform.origin.x = pos_x
+	object.transform.origin.y = pos_y
+	object.transform.origin.z = pos_z
 	
-	speed_x = rng.randf_range(-2.0, 2.0)
-	speed_y = rng.randf_range(-2.0, 2.0)
-	speed_z = rng.randf_range(-2.0, 2.0)
+	object.rotate_x(rng.randf_range(0, 3.2))
+	object.rotate_y(rng.randf_range(0, 3.2))
+	object.rotate_z(rng.randf_range(0, 3.2))
 	
-	rot_x = rng.randf_range(-2.0, 2.0)
-	rot_y = rng.randf_range(-2.0, 2.0)
-	rot_z = rng.randf_range(-2.0, 2.0)
+	speed_x = atan(speed_bound * rng.randfn() / (1 + pos_x))
+	speed_y = atan(speed_bound * rng.randfn() / (1 + pos_y))
+	speed_z = atan(speed_bound * rng.randfn() / (1 + pos_z))
+	
+	rot_x = rot_bound * rng.randfn()
+	rot_y = rot_bound * rng.randfn()
+	rot_z = rot_bound * rng.randfn()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -37,6 +53,6 @@ func _process(delta):
 	rotate_y(speed_y*delta)
 	rotate_z(speed_z*delta)
 	
-	object.rotate_x(rot_x)
-	object.rotate_y(rot_y)
-	object.rotate_z(rot_z)
+	object.rotate_x(rot_x*delta)
+	object.rotate_y(rot_y*delta)
+	object.rotate_z(rot_z*delta)
